ec3e00bf9be22b888fee23e4dc11a06eabcde20a
luowei <786407418@qq.com>
2018-06-26 12:02:31 +0800

    init

---
+++ src/Ali.php
@@ -0,0 +1,108 @@
+<?php
+namespace shangxin\yii_alipay;
+use shangxin\yii_alipay\lib\Tools;
+use yii\base\Component;
+class Ali extends Component {
+
+    /**
+     * @var string
+     * 支付宝分配给开发者的应用ID
+     */
+    public $app_id;
+
+    /**
+     * @var h5支付接口名称
+     * 接口名称
+     */
+    public $h5_pay_method = "alipay.trade.wap.pay";
+
+    /**
+     * @var string
+     * 请求使用的编码格式，如utf-8,gbk,gb2312等
+     */
+    public $charset = "utf-8";
+
+    /**
+     * @var string
+     * 商户生成签名字符串所使用的签名算法类型，目前支持RSA2和RSA，推荐使用RSA2
+     */
+    public $sign_type = "RSA";
+
+    /**
+     * @var string
+     * h5支付调用的接口版本，固定为：1.0
+     */
+    public $h5_pay_version = "1.0";
+
+    /**
+     * @var string
+     * 支付宝毁掉地址
+     */
+    public $notify_url;
+
+    public $biz_content;
+
+    /**
+     * @var array 
+     * 公共请求参数
+     */
+    public $common_params = [];
+    public $practiceParam = [];
+    public $request_params;
+
+    public $h5_pay_request_url = "https://openapi.alipay.com/gateway.do";
+
+    public $h5_pay_public_key_path;
+    public $h5_pay_private_key_path;
+
+    /**
+     * @var Tools Tools
+     */
+    public $tools;
+
+    public function __construct(array $config = [])
+    {
+        parent::__construct($config);
+        $this->tools = new Tools();
+        $this->common_params["app_id"] = $this->app_id;
+        $this->common_params["charset"] = $this->charset;
+        $this->common_params["sign_type"] = $this->sign_type;
+        $this->common_params["timestamp"] = date("Y-m-d H:i:s",time());
+    }
+
+
+    /**
+     * @param $body 对一笔交易的具体描述信息。如果是多种商品，请将商品描述字符串累加传给body。
+     * @param $subject  商品的标题/交易标题/订单标题/订单关键字等。
+     * @param $out_trade_no 商户网站唯一订单号
+     * @param $total_amount 订单总金额，单位为元，精确到小数点后两位，取值范围[0.01,100000000]
+     * @param $passback_params  公用回传参数，如果请求时传递了该参数，则返回给商户时会回传该参数。支付宝会在异步通知时将该参数原样返回。本参数必须进行UrlEncode之后才可以发送给支付宝
+     * @param string $product_code  销售产品码，商家和支付宝签约的产品码。该产品请填写固定值：QUICK_WAP_WAY
+     */
+    public function h5_pay($body,$subject,$out_trade_no,$total_amount,$passback_params,$product_code="QUICK_WAP_WAY"){
+
+//        $this->common_params["notify_url"] = $this->notify_url;
+        $this->common_params["method"] = $this->h5_pay_method;
+        $this->common_params["version"] = $this->h5_pay_version;
+        $this->practiceParam = [
+            "subject"=>$subject,
+            "out_trade_no"=>$out_trade_no,
+            "total_amount"=>$total_amount,
+            "product_code"=>$product_code
+        ];
+
+        if(trim($body)!=''){
+            $this->practiceParam['body']=$body;
+        }
+
+        $this->common_params["biz_content"] = json_encode($this->practiceParam);
+        $para_filter=$this->tools->paraFilterNew($this->common_params);
+        $sort=$this->tools->argSort($para_filter);
+        $query=$this->tools->createLinkstring($sort);
+        $this->commomParam['sign'] = $this->tools->rsaSign($query, $this->pay_path.$this->ali_pay_private_key_path);
+        $request_url = $this->h5_pay_request_url."?".http_build_query($this->common_params);
+    }
+
+
+
+}
---
+++ src/lib/Tools.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luowei
+ * Date: 2018/6/26
+ * Time: 11:58
+ */
+namespace shangxin\yii_alipay\lib;
+class Tools{
+
+    /**
+     * 除去数组中的空值和签名参数,但保留sign_type
+     * @param array $para 签名参数组
+     * @return mixed 去掉空值与签名参数后的新签名参数组
+     */
+    public function paraFilterNew($para) {
+        $para_filter = array();
+        while (list ($key, $val) = each ($para)) {
+            if($key == "sign" || $val == "")continue;
+            else	$para_filter[$key] = $para[$key];
+        }
+        return $para_filter;
+    }
+
+    /**
+     * 对数组排序
+     * @param array $para 排序前的数组
+     * @return mixed 排序后的数组
+     */
+    public function argSort($para) {
+        ksort($para);
+        reset($para);
+        return $para;
+    }
+
+    /**
+     * 把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
+     * @param array $para 需要拼接的数组
+     * @return mixed 拼接完成以后的字符串
+     */
+    public function createLinkstring($para) {
+        $arg  = "";
+        while (list ($key, $val) = each ($para)) {
+            $arg.=$key."=".$val."&";
+        }
+        //去掉最后一个&字符
+        $arg = substr($arg,0,count($arg)-2);
+
+        //如果存在转义字符，那么去掉转义
+        if(get_magic_quotes_gpc()){$arg = stripslashes($arg);}
+
+        return $arg;
+    }
+
+    /**
+     * RSA签名
+     * @param array $data 待签名数据
+     * @param string $private_key_path 商户私钥文件路径
+     * @return mixed 签名结果
+     */
+    public function rsaSign($data, $private_key_path) {
+        $priKey = file_get_contents($private_key_path);
+        $res = openssl_get_privatekey($priKey);
+        openssl_sign($data, $sign, $res);
+        openssl_free_key($res);
+        //base64编码
+        $sign = base64_encode($sign);
+        return $sign;
+    }
+
+}
